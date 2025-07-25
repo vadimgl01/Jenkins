8e44d689d53a1ed8ec6445511b7d95a0ccbdfff9
Vadim Glazman <vadimglazman@gmail.com>
2025-07-25 21:06:52 +0300

    Flask appl

---
+++ app.py
@@ -0,0 +1,10 @@
+# app/app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Docker CI!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
